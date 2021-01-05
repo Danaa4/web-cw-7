6c30ad17631656dc8a84225f72fa2c7a7dace3d4
Danaa4 <Danaa4@users.noreply.github.com>
2021-01-05 22:08:48 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,23 @@
 // قم بحل السؤال الأول هنا
+
+
+let pets = [
+    'cat','dog','bird','turtle','hamster'
+]
+
+pets.pop();
+pets.push("rabit");
+pets[2] = "monky"
+
+let cat = {
+ name: 'black',
+ age:'1',
+ breed:'korat',
+ 
+}
+
+cat.emoji = "🐱 "
+
+console.log(pets)
+console.log(cat.age)
+console.log(cat)
